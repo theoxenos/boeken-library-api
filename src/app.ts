968403417428a21669f13d7b2c952c0e63ebab3d
Theoxenos <theoxenos@pm.me>
2026-07-17 13:10:57 +0200

    feat: add cors and morgan middleware and drizzle-zod

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
 import router from './routes/index.ts';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api/', router);
 
