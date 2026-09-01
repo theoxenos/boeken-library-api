eaabd34f67cf55ac27bed8e39dd4a6b48655206a
Theoxenos <theoxenos@pm.me>
2026-09-01 14:52:49 +0200

    refactor: add DATABASE_URL env var to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-const {JWT_SECRET, HASH_SECRET} = process.env;
+const {JWT_SECRET, HASH_SECRET, DATABASE_URL} = process.env;
 
 if (!JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined');
@@ -7,7 +7,12 @@ if (!HASH_SECRET) {
     throw new Error('HASH_SECRET is not defined');
 }
 
+if (!DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined');
+}
+
 const hashSecret = HASH_SECRET;
 const jwtSecret = JWT_SECRET;
+const databaseUrl = DATABASE_URL;
 
-export {hashSecret, jwtSecret};
+export {hashSecret, jwtSecret, databaseUrl};
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import {drizzle} from "drizzle-orm/libsql";
 import {relations} from "./relations.ts";
+import {databaseUrl} from "../config/index.ts";
 
-export const db = drizzle(process.env.DATABASE_URL!, {logger: true, relations});
+export const db = drizzle(databaseUrl, {logger: true, relations});
